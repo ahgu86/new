cee486c3f8fff527e9bd7868e99b3119929f8ac5
ahgu86 <159117127+ahgu86@users.noreply.github.com>
2024-05-15 19:02:51 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,42 @@
+source_file = "user_demo.txt"
+final_file = "result.txt"
+favorite_list = [
+    "安徽卫视",
+    "安徽经济生活",
+    "安徽公共",
+    "安徽综艺",
+    "安徽综艺体育",
+    "CCTV-1",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-13",
+    "山东体育",
+    "江苏卫视",
+    "凤凰卫视",
+    "浙江卫视",
+    "湖南卫视",
+    "凤凰资讯",
+    "凤凰中文",
+    "翡翠台",
+]
+open_online_search = True
+favorite_page_num = 5
+default_page_num = 3
+urls_limit = 10
+open_sort = True
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
+open_subscribe = True
+subscribe_urls = [
+    "https://m3u.ibert.me/txt/fmml_dv6.txt",
+    "https://m3u.ibert.me/txt/o_cn.txt",
+    "https://m3u.ibert.me/txt/j_iptv.txt",
+    "https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/anhui/anhui.txt",
+]
+open_multicast = True
+region_list = ["安徽"]
+strict_match = False
