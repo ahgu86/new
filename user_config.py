86939d521040b27bca1569f6d2349e5d182e0109
ahgu86 <159117127+ahgu86@users.noreply.github.com>
2024-05-18 07:01:17 +0800

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -1,16 +1,29 @@
-source_file = "user_demo.txt"
+source_file =  "user_demo.txt"
 final_file = "result.txt"
 favorite_list = [
     "安徽卫视",
     "安徽经济生活",
     "安徽公共",
-    "安徽综艺",
+    "安徽影视",
     "安徽综艺体育",
+    "阜阳新闻",
+    "阜阳公共",
+    "阜阳教育",
+    "阜阳都市",
     "CCTV-1",
+    "CCTV-3",
     "CCTV-5",
     "CCTV-5+",
+    "CCTV-6",
     "CCTV-13",
+    "CCTV1",
+    "CCTV3",
+    "CCTV5",
+    "CCTV5+",
+    "CCTV6",
+    "CCTV13",
     "山东体育",
+    "五星体育",
     "江苏卫视",
     "凤凰卫视",
     "浙江卫视",
@@ -18,16 +31,24 @@ favorite_list = [
     "凤凰资讯",
     "凤凰中文",
     "翡翠台",
+    "劲爆体育",
+    "超级体育",
+    "精品体育",
+    "纬来篮球",
+    "咪咕体育",
+    "CHC家庭影院",
+    "CHC动作电影",
+    "CHC影迷电影",
 ]
-open_online_search = True
+open_online_search = False
 favorite_page_num = 5
 default_page_num = 3
-urls_limit = 10
+urls_limit = 15
 open_sort = True
 response_time_weight = 0.5
 resolution_weight = 0.5
 recent_days = 30
-ipv_type = "ipv4"
+ipv_type = "all"
 domain_blacklist = ["epg.pw"]
 url_keywords_blacklist = []
 open_subscribe = True
@@ -35,8 +56,10 @@ subscribe_urls = [
     "https://m3u.ibert.me/txt/fmml_dv6.txt",
     "https://m3u.ibert.me/txt/o_cn.txt",
     "https://m3u.ibert.me/txt/j_iptv.txt",
-    "https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/anhui/anhui.txt",
+    "https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/anhui/mobile.txt",
+    "https://fanmingming.com/txt?url=https://live.fanmingming.com/tv/m3u/ipv6.m3u",
+    "https://gitee.com/golden223/iptv/raw/master/iptv.txt",
+    "https://gitee.com/wuxiyihu/IPTV/blob/master/IPTV",
 ]
 open_multicast = True
 region_list = ["安徽"]
-strict_match = False
